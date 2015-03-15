bbc4e290ba442fbb135e76da20573f5af77a289a
Matt Ober <matt.ober3@gmail.com>
2015-03-14 19:14:32 -0500

    Added the Post View

---
+++ CampusExchange/CampusExchange/PostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostViewController.swift
+//  CampusExchange
+//
+//  Created by Matt on 3/14/15.
+//  Copyright (c) 2015 MattOber. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostViewController: UIViewController {
+    
+    @IBOutlet weak var titleField : UITextField!
+    @IBOutlet weak var authorField : UITextField!
+    @IBOutlet weak var ISBNField : UITextField!
+    @IBOutlet weak var courseField : UITextField!
+    @IBOutlet weak var conditionField : UITextField!
+    @IBOutlet weak var priceField : UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func addListing(){
+        NSLog("test")
+    }
+}
