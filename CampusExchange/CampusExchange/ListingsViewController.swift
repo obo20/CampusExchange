1b5699276f31d9bdfb6c86b9c5c2be6693ae2fb6
Matt Ober <matt.ober3@gmail.com>
2015-03-15 16:50:01 -0500

    added in finding of listing objects

--- CampusExchange/CampusExchange/ListingsViewController.swift
+++ CampusExchange/CampusExchange/ListingsViewController.swift
@@ -8,11 +8,35 @@
 
 import Foundation
 import UIKit
+import Parse
 
 class ListingsViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        var currentUser = PFUser.currentUser()
+        var currentUserId = currentUser.objectId
+        var query = PFQuery(className:"Listing")
+        query.whereKey("UserID", equalTo:currentUserId)
+        //query.whereKey("ListingStatus", equalTo: "1")
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]!, error: NSError!) -> Void in
+            if error == nil {
+                // The find succeeded.
+                println("Successfully retrieved \(objects.count) scores.")
+                // Do something with the found objects
+                if let objects = objects as? [PFObject] {
+                    for object in objects {
+                        println(object.objectId)
+                    }
+                }
+            }
+            else {
+                // Log details of the failure
+                println("Error: \(error) \(error.userInfo!)")
+            }
+        }
     }
+
+    
 }
