947446cf2acb3b728178497f6a30a189cc19bac8
Matt Ober <matt.ober3@gmail.com>
2015-03-16 17:36:30 -0500

    added frame for the search results

--- CampusExchange/CampusExchange/SearchListingsViewController.swift
+++ CampusExchange/CampusExchange/SearchListingsViewController.swift
@@ -8,9 +8,11 @@
 
 import Foundation
 import UIKit
+import Parse
 
 class SearchListingsViewController: UIViewController {
     
+    var searchResults: [PFObject] = []
     @IBOutlet weak var titleField : UITextField!
     @IBOutlet weak var authorField : UITextField!
     @IBOutlet weak var ISBNField : UITextField!
@@ -22,6 +24,36 @@ class SearchListingsViewController: UIViewController {
     }
     
     @IBAction func searchBooks(){
-        NSLog("test")
+        let title = self.titleField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as NSString
+        let author = self.authorField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as NSString
+        let ISBN = self.ISBNField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as NSString
+        let course = self.courseField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as NSString
+        var query = PFQuery(className:"Listing")
+        // Get listings originally posted with the current user's id
+        query.whereKey("Title", containsString: title)
+        query.whereKey("Author", containsString: title)
+        query.whereKey("ISBN", containsString: title)
+        query.whereKey("Course", containsString: title)
+        query.orderByDescending("createdAt")
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]!, error: NSError!) -> Void in
+            if error == nil {
+                // The find succeeded.
+                println("Successfully retrieved \(objects.count) listings.")
+                //clear out the results
+                self.searchResults.removeAll(keepCapacity: false)
+                if let objects = objects as? [PFObject] {
+                    for object in objects {
+                        self.searchResults.append(object)
+                    }
+                }
+                // Reload table data after background query is done
+                //self.tableView.reloadData()
+            } else {
+                // Log details of the failure
+                println("Error: \(error) \(error.userInfo!)")
+            }
+        }
     }
+    
 }
