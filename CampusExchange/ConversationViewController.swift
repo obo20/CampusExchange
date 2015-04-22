d377137c52b22f01f55368d94cfbee976a800093
Matt Ober <matt.ober3@gmail.com>
2015-04-21 20:19:49 -0500

    Added the ConversationViewController and the MessagesViewController. Removed references to the MessagingViewController, and added segues to the ConversationViewController

---
+++ CampusExchange/ConversationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ConversationViewController.swift
+//  CampusExchange
+//
+//  Created by Matt on 4/21/15.
+//  Copyright (c) 2015 MattOber. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Parse
+
+class ConversationViewController: UIViewController, UITableViewDelegate {
+    
+}
